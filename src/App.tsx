9fe0a2b517ef33436b6c1b4c47c208516783a24f
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-06-08 19:11:26 +0000

    Applying thinky's changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <h1 className="text-4xl font-bold text-center mb-4">Welcome to Fathym!</h1>
+          <p className="text-center">This is a sample application using React and Tailwind CSS.</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
